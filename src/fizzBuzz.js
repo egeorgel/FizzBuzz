c1c69e6ad77b7d1af1c90e98af5d2868919f6475
egeorgel <edgar.georgel@gmail.com>
2016-08-12 15:09:22 +0200

    documentation

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,14 +1,34 @@
-/*
-*	Version 1
-*/
+////
+////	VERSION 1
+////
+
+/**
+ *	Can transform a number in a string, if it's multiple of 3 return Fizz
+ * 	else number in string
+ *	@input int 
+ *	@output String
+ */
 function multipleOf3RetrunFizz(number) {
 	return (number%3 == 0 ? "Fizz" : number.toString() );
 }
 
+/**
+ *	Can transform a number in a string, if it's multiple of 5 return Buzz
+ *	else number in string
+ *	@input int 
+ *	@output String
+ */
 function multipleOf5RetrunBuzz(number) {
 	return (number%5 == 0 ? "Buzz" : number.toString() );
 }
 
+/**
+ *	Creat and return an array of lenght 100 
+ *	with value 1 to 100 
+ *	if it's multiple of 3 contain Fizz
+ *	if it's multiple of 5 contain Buzz
+ *	@output [String]
+ */
 function return100numbers() {
 	var result = new Array();
 	for (var i = 1; i <= 100; i++) {
@@ -17,9 +37,16 @@ function return100numbers() {
 	return result;
 }
 
-/*
-*	Version 2
-*/
+////
+////  Version 2
+////
+
+/**
+ *	Can transform a number in a string, if it's multiple of 5 && 3 return FizzBuzz
+ *	else number in string
+ *	@input int 
+ *	@output String
+ */
 function multipleOf3And5RetrunFizzBuzz(number) {
 	if (number.toString() !== multipleOf3RetrunFizz(number) &&
 		number.toString() !== multipleOf5RetrunBuzz(number) ) {
@@ -28,6 +55,14 @@ function multipleOf3And5RetrunFizzBuzz(number) {
 	return number.toString();
 }
 
+/**
+ *	Creat and return an array of lenght 100 
+ *	with value 1 to 100 
+ *	if it's multiple of 3 contain Fizz
+ *	if it's multiple of 5 contain Buzz
+ *	if it's multiple of 3 && 5 contain FizzBuzz
+ *	@output [String]
+ */
 function return100numbersV2() {
 	var result = [];
 	for (var i = 1; i <= 100; i++) {
@@ -42,9 +77,17 @@ function return100numbersV2() {
 	return result;
 }
 
-/*
-*	helper for Version 1 and 2
-*/
+/**
+ *	helper for Version 1 and 2
+ *	transform number in to string, 
+ *	if it's multiple of 3 contain Fizz
+ *	if it's multiple of 5 contain Buzz
+ *	if it's multiple of 3 contain Fizz
+ *	if it's multiple of 3 && 5 contain Fizz
+ *	else number in string
+ *	@input int 
+ *	@output String
+ */
 function helper(index) {
 	var number = multipleOf3RetrunFizz(index);
 	if (number !== index.toString() ) {	
